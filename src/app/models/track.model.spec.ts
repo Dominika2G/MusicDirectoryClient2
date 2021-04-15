27b205336c6d86312deb8c7af5345ec623f1efe2
Dominika2G <dominikagoralczyk5@gmail.com>
2021-04-15 13:30:11 +0200

    Remove unnecesarry elements

--- src/app/models/track.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Track } from './track.model';
-
-describe('Track', () => {
-  it('should create an instance', () => {
-    expect(new Track()).toBeTruthy();
-  });
-});
--- src/app/shared/author-filter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AuthorFilterPipe } from './author-filter.pipe';
-
-describe('AuthorFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new AuthorFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
